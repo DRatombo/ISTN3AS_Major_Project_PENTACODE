26bd30aa915ecdbb48013b1acf2c9c07535201aa
Dudu <ratombodm@gmail.com>
2026-06-09 19:01:05 +0200

    Updated Connection and data source

---
+++ Cafe101/dsCafe101Hub.cs
@@ -0,0 +1,15 @@
+﻿namespace Cafe101
+{
+
+
+    public partial class dsCafe101Hub
+    {
+    }
+}
+
+namespace Cafe101.dsCafe101HubTableAdapters {
+    
+    
+    public partial class CustomerTableTableAdapter {
+    }
+}
